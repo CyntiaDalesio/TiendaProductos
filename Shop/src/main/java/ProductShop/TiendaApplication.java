aa36668abd840a69766d348253aee85fef395875
CyntiaDalesio <cyntiam.135@gmail.com>
2021-12-07 20:13:45 -0300

    initial commit

---
+++ Shop/src/main/java/ProductShop/TiendaApplication.java
@@ -0,0 +1,13 @@
+package ProductShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApplication.class, args);
+	}
+
+}
---
+++ Shop/src/test/java/TiendaProducto/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaProducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
