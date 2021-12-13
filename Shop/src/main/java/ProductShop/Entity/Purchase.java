72deb1be71c710d0af1ebb0d89a1264310c05959
Nadim18-lab <nadincho18@gmail.com>
2021-12-13 20:06:29 -0300

    Purchase Entity

---
+++ Shop/src/main/java/ProductShop/Entity/Purchase.java
@@ -0,0 +1,68 @@
+
+package ProductShop.Entity;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Purchase {
+    
+    @Id
+    @GeneratedValue(generator ="uuid")
+    @GenericGenerator(name ="uuid",strategy = "uuid2")
+    private String Id;
+    
+    private Integer total;
+    private Date date;
+    private String code;
+
+    public Purchase(String Id, Integer total, Date date, String code) {
+        this.Id = Id;
+        this.total = total;
+        this.date = date;
+        this.code = code;
+    }
+
+    public Purchase() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+
+    
+    
+    
+}
