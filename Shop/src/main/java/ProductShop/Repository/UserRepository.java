754e56f6a0444c60c7021e1eeb9d6f18ac0c1b51
IvanJahan <ivanjahan14@gmail.com>
2021-12-13 21:17:11 -0300

    Update UserRepository.java

--- Shop/src/main/java/ProductShop/Repository/UserRepository.java
+++ Shop/src/main/java/ProductShop/Repository/UserRepository.java
@@ -1,18 +1,15 @@
-
 package ProductShop.Repository;
 
-import ProductShop.Entity.Usuario;
+import ProductShop.Entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Usuario, String>{
+public interface UserRepository extends JpaRepository<User, String> {
 
-    
-     //public List<User> findByNombre(String nombre);
+    public List<User> findByNombre(String nombre);
 
-    public Usuario findByUsername(String username);
-    
-    
+    public User findByUsername(String username);
+
 }
