9c5cec0f60b096a54dec54739e0310054370fb23
IvanJahan <ivanjahan14@gmail.com>
2021-12-14 21:12:17 -0300

    Update DetailsService.java

--- Shop/src/main/java/ProductShop/Service/DetailsService.java
+++ Shop/src/main/java/ProductShop/Service/DetailsService.java
@@ -1,12 +1,19 @@
 package ProductShop.Service;
 
+import ProductShop.Repository.DetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DetailsService {
     
+    @Autowired
+    private DetailsRepository detailsRepository;
     
-    
+    public void createDetail(){
+        
+        
+    }
     
     
 }
