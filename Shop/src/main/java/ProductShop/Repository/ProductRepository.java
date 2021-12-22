abbc0c8daa87bb70ed1c248d7ff0344a78492632
HernanRotellini <hernanrotellini@hotmail.com>
2021-12-22 18:21:39 -0300

    Productos

--- Shop/src/main/java/ProductShop/Repository/ProductRepository.java
+++ Shop/src/main/java/ProductShop/Repository/ProductRepository.java
@@ -1,16 +1,21 @@
 package ProductShop.Repository;
 
 import ProductShop.Entity.Product;
+import ProductShop.Enums.Category;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductRepository extends JpaRepository <Product, String>{
 
-//    public List<Product> findByCategory();     
-//    
-//    public List<Product> findByName(String name);       
-//    
+//    public ArrayList<Product> findByCategory(Category category);     
+  
+//    public ArrayList<Product> findByName(String name);       
+// 
     public List<Product> findByidProduct(String idProduct);
+    
+//    public ArrayList<Product> findByPrice(Double price);
 }
