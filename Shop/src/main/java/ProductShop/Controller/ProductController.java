fa790822225978e9c9048ae9db7d4987f1a7e129
Tomas Herrera <tomasherrera132@gmail.com>
2021-12-29 17:37:50 -0300

    modify Buttons redirect to modify html

--- Shop/src/main/java/ProductShop/Controller/ProductController.java
+++ Shop/src/main/java/ProductShop/Controller/ProductController.java
@@ -26,7 +26,7 @@ public class ProductController {
      
     @GetMapping("/product")
     public String Index(){
-        return "/index";
+        return "modify";
         
     }
     @PostMapping("/addproduct")
--- Shop/src/main/resources/static/assets/index.js
+++ Shop/src/main/resources/static/assets/index.js
@@ -1,11 +1,21 @@
 /*redirect login button*/
 let login = document.getElementById("signIn");
-login.addEventListener("click", ()=>{
-    location.href ="/login";
+login.addEventListener("click", () => {
+    location.href = "/login";
 })
 
 /*reditect to home page*/
-let home =  document.getElementById("home");
-home.addEventListener("click", ()=>{
-    location.href = "/";
+let home = document.querySelectorAll("#home");
+home.forEach((item) => {
+    item.addEventListener("click", () => {
+        location.href = "/";
+    })
+})
+
+let buttonsModify = document.querySelectorAll(".modify");
+
+buttonsModify.forEach((button) => {
+    button.addEventListener("click", () => {
+        location.href = "/product";
+    })
 })
