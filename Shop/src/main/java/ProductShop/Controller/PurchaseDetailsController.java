fea2c1234b1427d2e7fb7e66e44141037f1b5e67
IvanJahan <ivanjahan14@gmail.com>
2022-01-11 23:30:27 -0300

    Update PurchaseDetailsController.java

--- Shop/src/main/java/ProductShop/Controller/PurchaseDetailsController.java
+++ Shop/src/main/java/ProductShop/Controller/PurchaseDetailsController.java
@@ -1,12 +1,23 @@
 package ProductShop.Controller;
 
+import ProductShop.Service.PurchaseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PurchaseDetailsController {
     
+    @Autowired
+    private PurchaseDetailsService purchaseDetService;
     
     
+    @GetMapping("/purchaseDetail")
+    public String loadDetail(ModelMap model) {
+        
+        return "purchaseDetail.html";
+    }    
     
     
 }
