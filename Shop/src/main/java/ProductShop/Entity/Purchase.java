a1bef0de441bab694d43e195152dc1a5e92c1b90
Nadim18-lab <nadincho18@gmail.com>
2022-01-25 18:39:41 -0300

    Update Purchase.java

--- Shop/src/main/java/ProductShop/Entity/Purchase.java
+++ Shop/src/main/java/ProductShop/Entity/Purchase.java
@@ -30,8 +30,6 @@ public class Purchase {
     @Temporal(TemporalType.DATE)
     private Date date;
     
-//    @OneToOne
-//    private Product product;
     
     @ManyToOne
     private Usuario usuario;
