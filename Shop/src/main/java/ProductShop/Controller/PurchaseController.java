3bc3353c20af54a72f8ca7b9db1fa8753008f6f0
Nadim18-lab <nadincho18@gmail.com>
2022-01-25 21:34:40 -0300

    xd

--- Shop/src/main/java/ProductShop/Controller/PurchaseController.java
+++ Shop/src/main/java/ProductShop/Controller/PurchaseController.java
@@ -1,9 +1,13 @@
 package ProductShop.Controller;
 
+import ProductShop.Entity.Product;
 import ProductShop.Entity.Purchase;
 import ProductShop.Entity.PurchaseDetails;
+import ProductShop.Entity.Usuario;
+import ProductShop.Service.ProductService;
 import ProductShop.Service.PurchaseDetailsService;
 import ProductShop.Service.PurchaseService;
+import ProductShop.Service.UserService;
 import ProductShop.errores.ErrorServicio;
 import java.util.List;
 import java.util.Optional;
@@ -11,37 +15,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PurchaseController {
-
+    @Autowired
+    private UserService userService;
     @Autowired
     private PurchaseDetailsService purchaseDetService;
     @Autowired
     private PurchaseService purchaseService;
+    @Autowired
+    private ProductService productService;
 
-    @GetMapping("/purchase")
-    public String details() {
-        return "purchase.html";
+    @GetMapping("/purchase/{idProduct}")
+    public String details(ModelMap model,@PathVariable String idProduct) throws ErrorServicio {
+        try{
+        Product product = productService.findProductById(idProduct);
+        model.addAttribute("producto", product);
+        }catch(Exception e){
+            throw new ErrorServicio("producto no econtrado");
+        }
+        return "purchaseProduct.html";
     }
 
-    @PostMapping("/detail")
-    public String createDetail(ModelMap model, @RequestParam String idUser, @RequestParam String idProduct, @RequestParam String paymentMethod, @RequestParam Integer quantity) throws ErrorServicio {
+    @PostMapping("/purchase")
+    public String createDetail(ModelMap model, @RequestParam String idProduct, @RequestParam String paymentMethod, @RequestParam Integer cantity) throws ErrorServicio {
         try {
-            purchaseDetService.createDetailsPurchase(idProduct, idUser, quantity, paymentMethod);
+            Usuario user = userService.obtenerUsuarioSesion();
+            purchaseDetService.createDetailsPurchase(idProduct, user.getIdUser(), cantity, paymentMethod);
         } catch (ErrorServicio e) {
             throw new ErrorServicio("El detalle no fue creado");
         }
-        return "purchase.html";
+        return "purchaseProduct.html";
     }
 
     @GetMapping("/detailpurchase")
     public String showDetail(ModelMap model) {
         List<Purchase> purchase = purchaseService.showPurchase();
         model.put("purchase", purchase);
-        return "purchase.html";
+        return "purchaseProduct.html";
     }
 
     @PostMapping("detail/finished")
