3bc57b340e559995ae640044bb1d659dc09c3e66
CyntiaDalesio <cyntiam.135@gmail.com>
2022-01-31 21:55:25 -0300

    permisos filtros

--- Shop/src/main/java/ProductShop/Controller/PrincipalController.java
+++ Shop/src/main/java/ProductShop/Controller/PrincipalController.java
@@ -8,6 +8,7 @@ import ProductShop.Service.ProductService;
 import ProductShop.Service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
