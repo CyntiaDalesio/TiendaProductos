7f886e077e2933398e451277046ec327bc0acd23
CyntiaDalesio <cyntiam.135@gmail.com>
2022-02-01 21:29:10 -0300

    requiered

--- Shop/src/main/resources/static/assets/purchase.js
+++ Shop/src/main/resources/static/assets/purchase.js
@@ -1,9 +1,12 @@
 
 let input = document.querySelector(".input-cantity");
+
     input.addEventListener("change", function(){
         if(input.value % 1 == 0 ){
+            
             let valueProduct = document.getElementById("valueProduct").innerHTML;
-            let total = valueProduct * input.value;
+
+            let total = valueProduct * parseInt(input.value);
             console.log(total)
             let totalPurchase = document.getElementById("totalPurchase").innerHTML = total;
         }else{
